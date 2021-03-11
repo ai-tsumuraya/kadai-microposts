e79a028ad721559be3af597ebccd03a70ca0e993
ai.tsumuraya <aitsumuraya@gmail.com>
2021-03-11 16:34:21 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(10)
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = "Your account has been registered successfully."
+      redirect_to @user
+    else
+      flash.now[:danger] = 'Failed to register your account.'
+      render :new
+    end
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
