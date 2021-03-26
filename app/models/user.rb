42954c9ff466eff09d5d58dcb86d1ab4c62a26d4
ai.tsumuraya <aitsumuraya@gmail.com>
2021-03-26 01:59:59 +0000

    kadai-micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow 
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
   
   def follow(other_user)
     unless self == other_user
@@ -28,6 +30,21 @@ class User < ApplicationRecord
   end
   
   def feed_microposts
-    Microposts.where(user_id: self.following_ids + [self.id])
+    Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def favorite(liked_micropost)
+    self.favorites.find_or_create_by(micropost_id: liked_micropost.id)
+  end
+  
+  def unfavorite(liked_micropost)
+    favorite = self.favorites.find_by(micropost_id: liked_micropost.id) 
+    favorite.destroy if favorite
+  end
+  
+  def liked?(liked_micropost)
+    self.likes.include?(liked_micropost)
   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,18 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create] do
     member do 
-    get :followings
-    get :followers
+      get :followings
+      get :followers
+    end
   end
-end
+
+  resources :users, only: [:index, :show, :create] do
+    member do
+      get :likes
+    end
+  end
   
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 end
